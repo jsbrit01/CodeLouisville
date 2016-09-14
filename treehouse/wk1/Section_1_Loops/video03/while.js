3cfd565055134640367fda25bb9b784fec4a08d2
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-14 11:00:23 -0400

    Completed files in video03 directory

--- treehouse/wk1/Section_1_Loops/video03/while.js
+++ treehouse/wk1/Section_1_Loops/video03/while.js
@@ -1,3 +1,10 @@
-while ( ) {
-  
+
+//Generate random numbers until the computer correctly guesses the random numbers
+//When correctly guessed, print number of guesses it required
+var randNum1 = randomNumber(10000), randNum2, count = 0;
+while ( randNum1 !== randNum2 ) {
+  randNum2 = randomNumber(10000);
+  count++;
 }
+document.write("<p>Correct guess was: " + randNum1 + "</p>");
+document.write("<p>It took the computer " + count + " guesses before the correct guess</p>");
