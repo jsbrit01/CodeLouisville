7ac784a7b4be1057f43d657e0afe674474c56675
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-14 10:43:32 -0400

    Finished code in video02 directory

--- treehouse/wk1/Section_1_Loops/video02/script.js
+++ treehouse/wk1/Section_1_Loops/video02/script.js
@@ -1,3 +1,15 @@
-function randomNumber(upper) {
-  return Math.floor( Math.random() * upper ) + 1;
-}
+//Generate 10 random numbers
+//Wrap variables & function in an anonymous function to avoid crowding global namespace
+(function(){
+  var counter = 0;
+  var randNum;
+  while(counter < 10){
+    randNum = randomNumber(5);
+    document.write("Number " + (counter + 1) + " is: " + randNum);
+    counter++;
+  }
+  
+  function randomNumber(upper) {
+    return Math.floor( Math.random() * upper ) + 1;
+  }
+})(); //
