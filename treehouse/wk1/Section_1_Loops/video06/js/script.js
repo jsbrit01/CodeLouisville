0e9e930f72698219fe4498693fd04b161c52cb27
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-14 12:30:15 -0400

    File for video06 directory

--- treehouse/wk1/Section_1_Loops/video06/js/script.js
+++ treehouse/wk1/Section_1_Loops/video06/js/script.js
@@ -4,17 +4,23 @@ var guessCount = 0;
 var correctGuess = false;
 
 function getRandomNumber( upper ) {
-  var num = Math.floor(Math.random() * upper) + 1; 
+  var num = Math.floor(Math.random() * upper) + 1;
   return num;
 }
 
-do {
+//Implement a break statement to break out of loop if user guesses more than 5x
+while(guessCount <= 5) {
   guess = prompt('I am thinking of a number between 1 and 10. What is it?');
   guessCount += 1;
   if (parseInt(guess) === randomNumber) {
     correctGuess = true;
-  }  
-} while ( ! correctGuess )
-  
-document.write('<h1>You guessed the number!</h1>');
-document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+    break;
+  }
+}
+
+if(correctGuess){
+  document.write('<h1>You guessed the number!</h1>');
+  document.write('It took you ' + guessCount + ' tries to guess the number ' + randomNumber);
+} else{
+  document.write("<h1>Sorry, you didn't guess the number</h1>");
+}
