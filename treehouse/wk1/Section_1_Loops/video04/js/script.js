ee87d7f0d01c9e76ca2959da3b5fbe5e3a8cb0ab
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-14 11:45:26 -0400

    Completed files in video04 directory

--- treehouse/wk1/Section_1_Loops/video04/js/script.js
+++ treehouse/wk1/Section_1_Loops/video04/js/script.js
@@ -1,8 +1,21 @@
 var randomNumber = getRandomNumber(10);
 var guess;
 var guessCount = 0;
+var userNum;
 
 function getRandomNumber( upper ) {
   var num = Math.floor(Math.random() * upper) + 1;
   return num;
 }
+
+//Create program which asks user for a number which the computer
+//tries to guess.
+//Implement with a do...while loop
+userNum = parseInt(prompt("Enter a number between 1 & 10"));
+do{
+  guess = getRandomNumber(userNum);
+  guessCount++;
+}while(guess !== userNum);
+
+document.write("<p>User's number: " + userNum + "</p>");
+document.write("<p>Computer required " + guessCount + " guesses before correctly selecting the user's number</p>");
