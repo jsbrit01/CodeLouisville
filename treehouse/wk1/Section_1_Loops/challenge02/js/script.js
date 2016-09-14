6b4da9ede43b9cb2b6324c95318a38d84d47a07b
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-14 13:36:25 -0400

    Finished files in challenge02 directory

--- treehouse/wk1/Section_1_Loops/challenge02/js/script.js
+++ treehouse/wk1/Section_1_Loops/challenge02/js/script.js
@@ -3,13 +3,39 @@ var red;
 var green;
 var blue;
 var rgbColor;
+var colorRangeNum = 256;
+var body = document.getElementById('color');
 
-for (var i = 0; i < 10; i += 1) {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
+//Original code
+/*
+for(var i = 0; i < 100; i += 1){
+  red = Math.floor(Math.random() * 256);
+  green = Math.floor(Math.random() * 256);
+  blue = Math.floor(Math.random() * 256);
   rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  html += '<div style="background-color:' + rgbColor + '"></div>';
+  html += <div style="background-color:'" + rgbColor + "'></div>";
 }
 
 document.write(html);
+*/
+
+//Refactor above code to create divs with randomly generated colors
+//so that no code is repeated
+for (var i = 0; i < 10; i++) {
+  var div = document.createElement('div');
+  div.setAttribute('style', 'background-color:' + GenerateRGB(colorRangeNum));
+  body.appendChild(div);
+}
+
+function GenerateRGB(colorRangeNum){
+  var numArray = [];
+  for(y = 0; y < 3; y++){
+      numArray.push(GenerateRandNum(colorRangeNum));
+  }
+  rgbColor = 'rgb(' + numArray[0] + ',' + numArray[1] + ',' + numArray[2] + ')';
+  return rgbColor;
+}
+
+function GenerateRandNum(aNum){
+  return Math.floor(Math.random()*aNum);
+}
