fab1076043d07f64308a4ae88f6640f6edaf7dbf
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-15 18:15:17 -0400

    Finished Wk1, Section_2_Arrays, video06 files

--- treehouse/wk1/Section_2_Arrays/video06/js/search.js
+++ treehouse/wk1/Section_2_Arrays/video06/js/search.js
@@ -5,3 +5,54 @@ function print(message) {
   document.write( '<p>' + message + '</p>');
 }
 
+//Create function which allows user to search items in "inStock" array
+function ItemSearch(aString, anArray){
+  var index = anArray.indexOf(aString);
+  console.log(index);
+  var msg = "";
+  if(index === -1){
+    msg = "Item not in stock.";
+  } else {
+    msg = "Item available!";
+  }
+  return msg;
+}
+
+//Create function to display all values in "inStock"
+function DisplayStock(anArray){
+  return anArray.join(", ");
+}
+
+//Display controls
+var searchBtn = document.getElementById("searchBtn");
+var searchRdo = document.getElementById("searchRdo");
+var displayRdo = document.getElementById("displayRdo");
+var searchTxt = document.getElementById("searchBar");
+var searchResult = document.getElementById("searchResult");
+var searchLbl = document.getElementById("searchLbl");
+
+searchBtn.addEventListener("click", function(){
+  var msg = ItemSearch(searchTxt.value, inStock);
+  searchResult.innerHTML = msg;
+});
+
+searchRdo.addEventListener("click", function(){
+  //Display search bar & search button
+  searchTxt.style.display = "inline";
+  searchBtn.style.display = "inline";
+  searchLbl.style.display = "inline";
+
+  //Clear result area
+  searchResult.innerHTML = "";
+});
+
+displayRdo.addEventListener("click", function(){
+  //Hide search bar & search button
+  searchTxt.style.display = "none";
+  searchBtn.style.display = "none";
+  searchLbl.style.display = "none";
+
+  //Display all items in stock
+  var inStockItems = DisplayStock(inStock);
+  searchResult.innerHTML = inStockItems;
+});
