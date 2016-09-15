e352ce35c9ad92c7dfa526913678ef38640b9a11
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-15 17:06:21 -0400

    Finished Wk1, Section_2_Arrays, Video05

--- treehouse/wk1/Section_2_Arrays/video05/js/playlist.js
+++ treehouse/wk1/Section_2_Arrays/video05/js/playlist.js
@@ -7,7 +7,24 @@ var playList = [
   'Maybellene'
 ];
 
-function print(message) {
-  document.write(message);
+//Build a loop which creates an ordered list from the playlist array
+print(BuildList(playList));
+
+function BuildList(anArray){
+  //create list object
+  var ol = document.createElement("ol");
+  //loop through array
+  for(var i = 0; i < anArray.length; i++){
+    //create list item with song as inner html
+    var li = document.createElement("li");
+    li.innerHTML = anArray[i];
+    //Append list item to list
+    ol.appendChild(li);
+  }
+  //Return list object
+  return ol;
 }
 
+function print(orderedList) {
+  document.getElementById('playlist').appendChild(orderedList);
+}
