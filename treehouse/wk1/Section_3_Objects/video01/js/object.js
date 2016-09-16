ed26f86c06accbf468377fc1731f02c19b29dc8a
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-16 12:19:28 -0400

    Finished Wk1, Section_3_Objects, video01 files

---
+++ treehouse/wk1/Section_3_Objects/video01/js/object.js
@@ -0,0 +1,9 @@
+//Create a simple 'person' object
+
+var person = {
+  name: "John",
+  country: "US",
+  age: 30,
+  interests: ['scuba diving', 'flying planes', 'base jumping', 'bmxing'],
+  pet: {name: 'Smokey', type: 'bear', age: 6}
+};
