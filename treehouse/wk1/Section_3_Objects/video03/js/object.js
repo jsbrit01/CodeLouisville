90af70aac93477ec1750f07437118c0b3b53b7c7
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-16 13:00:50 -0400

    Completed Wk1, Section_3_Objects, video03 files

--- treehouse/wk1/Section_3_Objects/video03/js/object.js
+++ treehouse/wk1/Section_3_Objects/video03/js/object.js
@@ -3,6 +3,30 @@ var person = {
   country : 'US',
   age : 35,
   treehouseStudent : true,
-  skills : ['JavaScript', 'HTML', 'CSS']
+  skills : ['JavaScript', 'HTML', 'CSS'],
+  pet : {
+    name: "Smokey",
+    type: "bear"
+  }
 };
 
+//Use a for...in loop to iterate through the person object's properties
+//Print results out to the console
+function DisplayObject(anObj){
+  var count = 0;
+  for(var key in anObj){
+    if(Array.isArray(anObj[key])){
+      console.log(key, ":", anObj[key].join(", "));
+    } else if(typeof anObj[key] === 'object'){
+      console.log(key, " (sub-object): ")
+      DisplayObject(anObj[key]);
+      console.log("\n");
+    } else {
+      console.log(key, ": ", anObj[key]);
+    }
+    count++;
+  }
+  console.log("The object has ", count, " total properties");
+}
+
+DisplayObject(person);
