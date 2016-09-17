daa93a4d8dd0bea35c5cd871d26c7746b89efe21
Joshua Britton <jsbrit01@users.noreply.github.com>
2016-09-17 17:08:43 -0400

    Finished wk2, section2 'Perfect' lesson

--- treehouse/wk2/section2-creating-a-spoiler-revealer/mySolution/js/app.js
+++ treehouse/wk2/section2-creating-a-spoiler-revealer/mySolution/js/app.js
@@ -6,12 +6,12 @@
 // 1) Hide spoiler by default
 var $spoilerSpan = $(".spoiler > span").hide();
 // 2) Add button
-var $button = $("<button></button>").text("Reveal Spoilers");//.after($spoiler);
+var $button = $("<button></button>").text("Reveal Spoilers");
 $spoilerSpan.after($button);
 // 3) When button clicked:
-$button.on("click", function(){
+$("button").on("click", function(){
   // 3.1) Reveal spoiler
-  $spoilerSpan.show();
+  $(this).prev().show();
   // 3.2) Hide button
   $(this).hide();
 });
